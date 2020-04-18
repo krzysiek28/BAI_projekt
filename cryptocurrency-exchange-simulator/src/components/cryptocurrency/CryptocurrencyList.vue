1b1f7835a9002e7a6453217009dd510a21121a22
MonikaSajbura <monika.sajbura@gmail.com>
2020-04-18 18:36:11 +0200

    Update CryptocurrencyList.vue
    First changes - table with currencies

--- cryptocurrency-exchange-simulator/src/components/cryptocurrency/CryptocurrencyList.vue
+++ cryptocurrency-exchange-simulator/src/components/cryptocurrency/CryptocurrencyList.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="cryptocurrencyList">
+
     <b-container class="bv-example-row">
-      <b-row cols="1" cols-sm="2" cols-md="4" cols-lg="4">
-        <b-col>Kryptowaluta</b-col>
-        <b-col>Kurs</b-col>
-        <b-col>Kupno</b-col>
-        <b-col>Sprzedaż</b-col>
-      </b-row>
-      <b-row cols="1" cols-sm="2" cols-md="4" cols-lg="4">
+      <b-row cols="1" cols-sm="1" cols-md="1" cols-lg="1">
+        <b-col>
+          <b-table striped hover :items="currency" ></b-table>
+        </b-col>
       </b-row>
     </b-container>
   </div>
 </template>
-
 <script>
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class CryptocurrencyList extends Vue {
-
+  data () {
+    return {
+      currency: [
+        { cryptocurrency: 'BitCoin', exchange: 150, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' },
+        { cryptocurrency: 'BitCoin2', exchange: 155, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' },
+        { cryptocurrency: 'BitCoin3', exchange: 151, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' },
+        { cryptocurrency: 'BitCoin4', exchange: 152, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' }
+      ]
+    }
+  }
 }
 </script>
 
