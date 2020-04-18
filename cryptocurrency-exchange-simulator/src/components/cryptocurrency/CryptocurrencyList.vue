756645c43de71e27029be744e345dca5af285032
MonikaSajbura <monika.sajbura@gmail.com>
2020-04-18 18:50:49 +0200

    Layout of home page
    add table

--- cryptocurrency-exchange-simulator/src/components/cryptocurrency/CryptocurrencyList.vue
+++ cryptocurrency-exchange-simulator/src/components/cryptocurrency/CryptocurrencyList.vue
@@ -7,6 +7,16 @@
           <b-table striped hover :items="currency" ></b-table>
         </b-col>
       </b-row>
+      <b-row cols="1" cols-sm="1" cols-md="1" cols-lg="1">
+        <b-col>
+          <h2>Zarządzaj listą</h2>
+        </b-col>
+      </b-row>
+      <b-row cols="1" cols-sm="1" cols-md="1" cols-lg="1">
+        <b-col>
+          <b-table striped hover :items="othercurrency" ></b-table>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -22,6 +32,12 @@ export default class CryptocurrencyList extends Vue {
         { cryptocurrency: 'BitCoin2', exchange: 155, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' },
         { cryptocurrency: 'BitCoin3', exchange: 151, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' },
         { cryptocurrency: 'BitCoin4', exchange: 152, buy: 'KUP', sell: 'SPRZEDAJ', details: 'Szczegóły' }
+      ],
+      othercurrency: [
+        { cryptocurrency: 'BitCoin', details: 'Dodaj/usuń' },
+        { cryptocurrency: 'BitCoin2', details: 'Dodaj/usuń' },
+        { cryptocurrency: 'BitCoin3', details: 'Dodaj/usuń' },
+        { cryptocurrency: 'BitCoin4', details: 'Dodaj/usuń' }
       ]
     }
   }
