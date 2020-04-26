c139028b68ec345ea2acb787b5ea983924135f21
Joanna Romańska <37107159+JoannaRomanska@users.noreply.github.com>
2020-04-26 20:08:29 +0200

    Update BalanceInfo.vue

--- cryptocurrency-exchange-simulator/src/components/wallet/BalanceInfo.vue
+++ cryptocurrency-exchange-simulator/src/components/wallet/BalanceInfo.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="balanceInfo">
-    <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-card no-body class="mx-auto" style="max-width: 540px;">
       <b-row no-gutters>
-        <b-col md="3">
+        <b-col md="6">
           <b-card-body title="Balance">
             <b-card-text>{{this.balance.toFixed(2)}}</b-card-text>
           </b-card-body>
         </b-col>
-        <b-col md="3">
+        <b-col md="6">
           <b-card-body title="Profit">
             <b-card-text>{{this.profit.toFixed(2)}}</b-card-text>
           </b-card-body>
         </b-col>
-        <b-col md="3">
+      </b-row>
+    </b-card>
+    <b-card no-body class="mx-auto" style="max-width: 540px;">
+      <b-row no-gutters>
+        <b-col md="6">
           <b-card-body title="Invested money">
             <b-card-text>{{this.investedMoney.toFixed(2)}}</b-card-text>
           </b-card-body>
         </b-col>
-        <b-col md="3">
+        <b-col md="6">
           <b-card-body title="Income or loss">
             <b-card-text>{{this.incomeOrLoss.toFixed(2)}}</b-card-text>
           </b-card-body>
         </b-col>
       </b-row>
     </b-card>
-
-    <h4>Posiadane kryptowaluty:</h4>
+    <br>
+    <h4><b>Posiadane kryptowaluty:</b></h4>
     <b-container class="bv-example-row">
       <b-row cols="1" cols-sm="1" cols-md="1" cols-lg="1">
         <b-col>
@@ -34,7 +38,7 @@
         </b-col>
       </b-row>
     </b-container>
-
+  <br>
   </div>
 </template>
 
